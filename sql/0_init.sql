fe1dfecf11c8606e5454e3b4b8ada9d294017ea5
Daino <dailory98@gmail.com>
2021-04-02 19:24:14 +0200

    Update init.sql

--- sql/0_init.sql
+++ sql/0_init.sql
@@ -2,3 +2,58 @@ DROP SCHEMA IF EXISTS pokedex CASCADE;
 CREATE SCHEMA pokedex;
 
 SET search_path TO pokedex;
+
+create table pokemons
+(
+    id             serial not null,
+    name           varchar(255),
+    primary_type   varchar(255),
+    secondary_type varchar(255)
+);
+
+create table trainers
+(
+    id            serial not null,
+    first_name    varchar(255),
+    last_name     varchar(255),
+    birth_date    timestamp,
+    gender        char,
+    birth_country varchar(255)
+);
+
+create table pokemon_trainer
+(
+    pokemon_id    integer,
+    trainer_id    integer,
+    is_gym_leader boolean,
+    nickname      varchar(255),
+    gender        char,
+    level         integer,
+    catch_date    timestamp
+);
+
+create table battles
+(
+    id                serial not null,
+    first_trainer_id  integer,
+    second_trainer_id integer,
+    battle_date       timestamp
+);
+
+create table gyms
+(
+    id         serial not null,
+    region     varchar(255),
+    type       varchar(255),
+    medal_name varchar(255),
+    gym_leader integer
+);
+
+create table trainer_gym
+(
+    trainer_id   integer,
+    gym_id       integer,
+    last_attempt timestamp,
+    has_won      boolean
+);
+
