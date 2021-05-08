e7bae311f88cfc1bcc4cb13c7e87c5212f61a477
Daino <dailory98@gmail.com>
2021-05-08 12:02:36 +0200

    Inizio parte D

---
+++ sql/3.0_roles.sql
@@ -0,0 +1,37 @@
+-- Parte D
+
+-- Allenatore semplice: puo interagire con pokemon_trainer e battles in scrittura, e con trainers, pokemons e trainer_gym in lettura
+-- Capopalestra: come trainer, ma può scrivere in trainer_gym
+-- Professore: può leggere tutto, scrivere in pokemons (nuova specie scoperta), trainers (nuovo allenatore registrato), battles
+-- Dio: può fare qualsiasi cosa, equivale ad admin
+
+CREATE ROLE trainer;
+CREATE ROLE gym_leader;
+CREATE ROLE professor;
+CREATE ROLE god;
+
+CREATE USER generic_trainer WITH ROLE trainer;
+CREATE USER other_generic_trainer WITH ROLE trainer;
+CREATE USER generic_gym_leader WITH ROLE gym_leader;
+CREATE USER generic_professor WITH ROLE professor;
+CREATE USER arceus WITH ROLE god;
+
+-- Ruoli trainer
+GRANT SELECT, INSERT, UPDATE, DELETE ON pokedex.pokemon_trainer TO trainer;
+GRANT SELECT, INSERT ON pokedex.battles TO trainer;
+GRANT SELECT ON pokedex.trainers TO trainer;
+GRANT SELECT ON pokedex.trainer_gym TO trainer;
+
+-- Ruoli gym_leader
+GRANT trainer to gym_leader;
+GRANT INSERT, UPDATE ON pokedex.trainer_gym TO trainer;
+
+--Ruoli professor
+GRANT SELECT ON ALL TABLES IN SCHEMA pokedex TO professor;
+GRANT INSERT, UPDATE ON pokedex.pokemons TO professor;
+GRANT INSERT, UPDATE ON pokedex.trainers TO professor;
+GRANT INSERT ON pokedex.battles TO professor;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA pokedex TO god;
+
+--TODO punto 4 e 5
