a6abe0f06a87f3bf9ad8792ade8be720ff1a5802
Mattia Dapino <mattia.dapino@hey.com>
2021-05-30 18:30:26 +0200

    Restart db before query inspection (no cache)

--- scripts/relazione.js
+++ scripts/relazione.js
@@ -1,74 +1,111 @@
-const fs = require('fs');
-const path = require('path');
-const childProcess = require('child_process');
-const { mdToPdf } = require('md-to-pdf');
-const { mainModule } = require('process');
-
+const fs = require("fs");
+const path = require("path");
+const childProcess = require("child_process");
+const { mdToPdf } = require("md-to-pdf");
+const { mainModule } = require("process");
 
 /** CONFIG **/
-const srcPath = './docs/';
-const destName = './Relazione_BD2_Dainelli_Dapino.pdf';
+const srcPath = "./docs/";
+const destName = "./Relazione_BD2_Dainelli_Dapino.pdf";
 /** END CONFIG **/
-
 
 const workdir = path.join(process.cwd(), srcPath);
 
+function restartDb() {
+  childProcess.spawnSync("docker-compose", ["down"], {
+    env: process.env,
+    cwd: process.cwd(),
+  });
+  childProcess.spawnSync("docker-compose", ["up", "-d"], {
+    env: process.env,
+    cwd: process.cwd(),
+  });
+  childProcess.spawnSync("sleep", ["5"]);
+}
 
 function runQuery(query) {
-  return childProcess.spawnSync('docker-compose',
-    ['exec', '-T', 'postgres', 'psql', '-U', 'postgres', '-P', 'pager=off', '-H', '-c', query],
+  console.log("Running", query);
+  return childProcess.spawnSync(
+    "docker-compose",
+    [
+      "exec",
+      "-T",
+      "postgres",
+      "psql",
+      "-U",
+      "postgres",
+      "-P",
+      "pager=off",
+      "-H",
+      "-c",
+      query,
+    ],
     { env: process.env, cwd: process.cwd() }
   );
 }
 
-// NON FARE ASYNC, 
-//   LA GENERAZIONE DEVE ESSERE OBBLICATORIAMENTE SINCRONA PER GARANTIRE ORDINE DI ESECUZIONE
-async function processString({file, md}) {
+function processString({ file, md }) {
   console.log("Started ", file);
-  const res = md.replace(/^@query\((.+)\)$/gm, function (match, p1) {
-    return runQuery(p1).stdout.toString();
-  }).replace(/^@runQuery\((.+)\)$/gm, function (match, p1) {
-    console.log(runQuery(p1).stdout.toString());
-    return '';
-  }).replace(/^@queryFile\((.+)\)$/gm, function (match, p1) {
-    const filePath = path.join(workdir, p1);
-    const query = fs.readFileSync(filePath).toString();
-    return runQuery(query).stdout.toString();
-  }).replace(/^@qiFile\((.+)\)$/gm, function (match, p1) {
-    const filePath = path.join(workdir, p1);
-    const query = 'EXPLAIN ANALYZE ' + fs.readFileSync(filePath).toString();
-    return runQuery(query).stdout.toString();
-  }).replace(/^@runQueryFile\((.+)\)$/gm, function (match, p1) {
-    const filePath = path.join(workdir, p1);
-    const query = fs.readFileSync(filePath).toString();
-    console.log(runQuery(query).stdout.toString());
-    return '';
-  }).replace(/^@file\((.+)\)$/gm, function (match, p1) {
-    const filePath = path.join(workdir, p1);
-    return fs.readFileSync(filePath).toString();
-  });
+  const res = md
+    .replace(/^@query\((.+)\)$/gm, function (match, p1) {
+      return runQuery(p1).stdout.toString();
+    })
+    .replace(/^@runQuery\((.+)\)$/gm, function (match, p1) {
+      console.log(runQuery(p1).stdout.toString());
+      return "";
+    })
+    .replace(/^@queryFile\((.+)\)$/gm, function (match, p1) {
+      const filePath = path.join(workdir, p1);
+      const query = fs.readFileSync(filePath).toString();
+      return runQuery(query).stdout.toString();
+    })
+    .replace(/^@qiFile\((.+)\)$/gm, function (match, p1) {
+      restartDb();
+      const filePath = path.join(workdir, p1);
+      const query = "EXPLAIN ANALYZE " + fs.readFileSync(filePath).toString();
+      return runQuery(query).stdout.toString();
+    })
+    .replace(/^@runQueryFile\((.+)\)$/gm, function (match, p1) {
+      const filePath = path.join(workdir, p1);
+      const query = fs.readFileSync(filePath).toString();
+      console.log(runQuery(query).stdout.toString());
+      return "";
+    })
+    .replace(/^@file\((.+)\)$/gm, function (match, p1) {
+      const filePath = path.join(workdir, p1);
+      return fs.readFileSync(filePath).toString();
+    });
   console.log("Finished ", file);
 
   return res;
 }
 
-
 (async function () {
-  const mds = fs.readdirSync(workdir)
-    .filter(f => f.endsWith('.md'))
-    .map(f => {
+  const start = new Date();
+
+  const md = fs
+    .readdirSync(workdir)
+    .filter((f) => f.endsWith(".md"))
+    .map((f) => {
       return {
         file: f,
-        md: fs.readFileSync(path.join(workdir, f)).toString()
-      }
-    });
+        md: fs.readFileSync(path.join(workdir, f)).toString(),
+      };
+    })
+    .map(processString)
+    .join("\n\n");
 
-  const md = (await Promise.all(mds.map(processString))).join('\n\n');
+  mdToPdf(
+    { content: md },
+    {
+      dest: destName,
+      basedir: workdir,
+      // as_html: true,
+      css: "table td { padding: 0 !important; }",
+    }
+  );
 
-  mdToPdf({ content: md }, { 
-    dest: destName,
-    basedir: workdir,
-    // as_html: true,
-    css: 'table td { padding: 0 !important; }'
-  });
+  const end = new Date();
+  const diff = Math.round((end - start) / 1000);
+  console.log(`Terminato in ${Math.round(diff / 60)}m ${diff % 60}s`);
 })();
