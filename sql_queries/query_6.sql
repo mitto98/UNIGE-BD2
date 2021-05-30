dfd1e0d6d2b0053c09b735ddb7211167cd255e6d
Mattia Dapino <mattia.dapino@hey.com>
2021-05-30 18:42:30 +0200

    Modifica query 6

--- sql_queries/query_6.sql
+++ sql_queries/query_6.sql
@@ -1,4 +1,4 @@
 SELECT t.id, t.first_name, t.last_name, b.id
-FROM (SELECT * FROM pokedex.trainers WHERE last_name = 'Panini') t 
+FROM (SELECT id, first_name, last_name FROM pokedex.trainers WHERE last_name = 'Panini') t
   JOIN pokedex.battles AS b ON b.first_trainer_id = t.id
 WHERE b.win;
