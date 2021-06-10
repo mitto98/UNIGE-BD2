5020ecc7d125ba1a158474f4d8c5dfa61025a3f0
Daino <dailory98@gmail.com>
2021-06-10 21:08:49 +0200

    Aggiunte parole chiave di transazioni

--- sql_queries/transactions.sql
+++ sql_queries/transactions.sql
@@ -1,6 +1,8 @@
-set search_path to pokedex;
+SET SEARCH_PATH TO pokedex;
 
 -- Transaction 1
+BEGIN;
+
 SELECT name
 FROM pokemons
 WHERE primary_type = 'normal';
@@ -13,7 +15,11 @@ SELECT name, primary_type
 FROM pokemons
 WHERE name = 'JUST_INVENTED';
 
+COMMIT;
+
 -- Transaction 2
+BEGIN;
+
 DELETE
 FROM trainer_gym
 WHERE has_won IS FALSE
@@ -26,7 +32,11 @@ WHERE name IN (SELECT pokemon FROM pokemon_trainer WHERE level = 1);
 INSERT INTO trainers (first_name, last_name, birth_date, gender, birth_country, is_gym_leader)
 VALUES ('Ajeje', 'Brazorf', '1998/04/20', 'M', 'Kalos', TRUE);
 
+COMMIT;
+
 -- Transaction 3
+BEGIN;
+
 SELECT *
 FROM trainer_gym
 WHERE has_won IS FALSE
@@ -41,7 +51,11 @@ FROM trainers
 WHERE is_gym_leader IS TRUE
   AND birth_country = 'Kalos';
 
+COMMIT;
+
 -- Transaction 4
+BEGIN;
+
 SELECT name, primary_type
 FROM pokemons
 WHERE name = 'JUST_INVENTED';
@@ -52,3 +66,5 @@ VALUES ('JUST_INVENTED', 'normal', 'THIS_IS_A_SPRITE');
 SELECT first_name, last_name
 FROM trainers
 WHERE username = (SELECT trainer FROM pokemon_trainer GROUP BY trainer ORDER BY COUNT(*) DESC LIMIT 1);
+
+COMMIT;
